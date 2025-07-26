1b3a51cbb59a279435399dcdbf50c6d677a5e641
haroon43-oss <haroon.hnpwd1@gmail.com>
2025-07-26 10:55:55 +0500

    Add files via upload

---
+++ sticky.js
@@ -0,0 +1,11 @@
+
+        window.addEventListener('scroll', function () {
+            var header = document.querySelector('header');
+            header.classList.toggle("sticky", window.scrollY > 0);
+        });
+
+        function toggle() {
+            var header = document.querySelector('header');
+            header.classList.toggle("active");
+            toggle.classList.toggle('active');
+        }
